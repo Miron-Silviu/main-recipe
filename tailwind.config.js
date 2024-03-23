3d72cf5d6c97797199e2add5cf27ba1a4eb1e6dd
Silviu Miron <silviu_miron_90@outlook.com>
2024-03-23 22:26:26 +0200

    Build :Add colors in tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./docs/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // primary and secondary
+        nutmeg: 'hsl(14, 45%, 36%)',
+        darkRaspberry: 'hsl(332, 51%, 32%)',
+
+        //neural
+        white: 'hsl(0, 0%, 100%)',
+        roseWhite: 'hsl(330, 100%, 98%)',
+        eggShall: 'hsl(30, 54%, 90%)',
+        lightGray: 'hsl(30, 18%, 87%)',
+        wageBrown: 'hsl(30, 10%, 34%)',
+        darkCharcoal: 'Dark Charcoal',
+      },
+    },
   },
   plugins: [],
 };
